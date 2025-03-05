f60050da1a25fb256baa43d7c52b386bff0f9c0b
rpjarmusz <jarmusz.r@northeastern.edu>
2025-03-04 21:15:49 -0500

    Modified the rob class to match example code

--- Lab_6/src/lab_6/TeamGreeting.java
+++ Lab_6/src/lab_6/TeamGreeting.java
@@ -17,8 +17,7 @@ public class TeamGreeting {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Member_Rob m1 = new Member_Rob();
-        m1.greeting();
+        Member_Rob.greeting();
         Member_Patrick m2 = new Member_Patrick();
         m2.greeting();
         Member_Zefan m3=new Member_Zefan();
